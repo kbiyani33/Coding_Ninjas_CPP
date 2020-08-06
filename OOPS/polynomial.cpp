0475f417f1075f15f0f277503d60d9b9b140b66d
kbiyani33 <54775827+kbiyani33@users.noreply.github.com>
2020-08-06 17:32:17 +0530

    Create polynomial.cpp

---
+++ OOPS/polynomial.cpp
@@ -0,0 +1,61 @@
+class Polynomial {
+    int *degCoeff;// Name of your array (Don't change this)
+    int capacity;
+    int next;
+    
+    
+    // Complete the class
+    public:
+    
+    Polynomial(){
+        degCoeff=new int(5);
+        for(int i=0;i<5;i++){
+            defCoeff[i]=0;
+        }
+        next=0;
+        capacity=5;
+    }
+    
+    void print(){
+        for(int i=0;i<next;i++){
+            if(DegCoeff[i]!=0){
+                cout<<DegCoeff[i]<<"x"<<i;
+            }
+        }
+        cout<<endl;
+    }
+    
+    Polynomial(Polynomial const &p2){
+        this -> DegCoeff = new int(p2.capacity);
+        for(int i=0;i<p2.next;i++){
+            this -> DegCoeff[i]=p2.DegCoeff[i];
+        }
+        this -> capacity=p2.capacity;
+        this -> next=p2.next;
+    }
+    
+    void operator=(Polynomial const &p2){
+        this -> DegCoeff = new int(p2.capacity);
+        for(int i=0;i<p2.next;i++){
+            this -> DegCoeff[i]=p2.DegCoeff[i];
+        }
+        this -> capacity=p2.capacity;
+        this -> next=p2.next;
+    }
+    
+    Polynomial operator*(Polynomial const &p){
+        Polynomial p3;
+        for(int i=0;i<capacity;i++)
+        {
+            for(int j=0;j<p.capacity;j++)
+            {
+                int degree= i+j;
+                int coeff= p3.get(degree)+ degCoeff[i]*p.degCoeff[j];
+                p3.setCoefficient(degree, coeff);
+            }
+        }
+        return p3;
+    }
+    
+    
+};
