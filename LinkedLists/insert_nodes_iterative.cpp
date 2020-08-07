3779e539517ca0ec0b7fc04992cd5e24ac6b1b37
kbiyani33 <54775827+kbiyani33@users.noreply.github.com>
2020-08-07 22:31:40 +0530

    Update insert_nodes_iterative.cpp

--- LinkedLists/insert_nodes_iterative.cpp
+++ LinkedLists/insert_nodes_iterative.cpp
@@ -12,15 +12,17 @@ Node* insert(Node *head , int i, int d){
     else
     {
         int a=1;//reason, we take 1 is because the 0th is already taken into account
-        while(a<i){
+        while(a<i && temp != NULL){//We have to make sure that the variable just before the -> or . is not NULL. otherwise we will get segmentation fault core dumped
             temp = temp -> next;
             a+=1;
         }
     //Now we will be getting the node address of node one before position i in temp
     //Now next of temp is the newnode and next of new node is the 
+    if(temp != NULL){//if input is greater than required, then don't do anything
         Node * ip=temp -> next;
         temp -> next = newnode;
         newnode -> next = ip;
+    }
     }
     return head;
 }
